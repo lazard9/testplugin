41d98bdc8de37606b11694d5039affe7edfcf754
Lazar Dacic <54157852+lazard9@users.noreply.github.com>
2019-11-04 17:11:57 +0100

    Update uninstall.php

--- fstp-custom-post/uninstall.php
+++ fstp-custom-post/uninstall.php
@@ -11,14 +11,14 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 // Clear Database stored data
-$portfolios = get_posts( array( 'post_type' => 'portfolio', 'numberposts' => -1 ) );
+$portfolios = get_posts( array( 'post_type' => 'estates', 'numberposts' => -1 ) );
 
-foreach( $portfolios as $portfolio ) {
-	wp_delete_post( $portfolio->ID, true );
+foreach( $portfolios as $estates ) {
+	wp_delete_post( $estates->ID, true );
 }
 
 // Access the database via SQL
 //global $wpdb;
-//$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'portfolio'" );
+//$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'estates'" );
 //$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
 //$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
